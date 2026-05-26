2073e170cc119ecef2b39566917a9b55271a8386
J.Thibakar <thiba2006077@gmail.com>
2026-05-26 20:45:54 +0530

    Create LogicalOperator.java

---
+++ Task10_Logical Operators/LogicalOperator.java
@@ -0,0 +1,11 @@
+class LogicalOperator {
+	public void main(String args[]) {
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println("a && b : " + (a && b)); //AND
+		System.out.println("a || b : " + (a || b)); //OR 
+		System.out.println("!a : " + (!a)); //NOT
+		
+	}
+}
