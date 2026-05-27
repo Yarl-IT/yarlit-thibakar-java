14327c125cf3ff9d61808708213c4e34de240dcf
J.Thibakar <thiba2006077@gmail.com>
2026-05-27 09:35:58 +0530

    Create IncrementOperators.java

---
+++ Task11_Increment operator/IncrementOperators.java
@@ -0,0 +1,24 @@
+class IncrementOperators {
+	public static void main(String args[]) {
+		
+		int x = 10;
+		System.out.println(x);
+		
+		x = x + 1;
+		System.out.println(x);
+		
+		x += 1;
+		System.out.println(x);
+		
+		x++;                     // Post-increment
+		System.out.println(x);
+		
+		++x;                    //  Pre-increment
+		System.out.println(x);
+		
+		System.out.println(++x);
+		System.out.println(x++);
+		System.out.println(x);
+		
+	}
+}
