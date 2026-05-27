2e727001a2266098a8401c13f43cb29b4891914e
J.Thibakar <thiba2006077@gmail.com>
2026-05-27 10:40:38 +0530

    Create marks1.java

---
+++ Task15_students pass or fail/marks1.java
@@ -0,0 +1,12 @@
+class marks1 {
+	public static void main(String args[]) {
+		int marks = 80;
+		if(marks > 75) {
+			System.out.println("Pass");
+		}
+		else {
+			System.out.println("Fail");
+		}
+			
+	}
+}
