7442094b2715e3cfeb6417c48a39863a919cf5f7
J.Thibakar <thiba2006077@gmail.com>
2026-05-27 10:51:46 +0530

    Create TernaryOperator.java

---
+++ task18_Ternery operator/TernaryOperator.java
@@ -0,0 +1,10 @@
+class TernaryOperator {
+	public static void main(String args[]) {
+		int x = 55;
+		String result;
+		
+		result = (x >= 50)?"Pass":"Fail";
+		
+		System.out.println(result);
+	}
+}
