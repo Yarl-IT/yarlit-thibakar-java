36d5e80a67e689095297ab2c2bf00105a6b334f0
J.Thibakar <thiba2006077@gmail.com>
2026-06-04 11:51:25 +0530

    Create array_ex1.java

---
+++ task27_Array declartion, Initialization & access/array_ex1.java
@@ -0,0 +1,15 @@
+class array_ex1{
+	public static void main(String args[]) {
+		int[] y;
+		y = new int[7];
+		y[0]=10;
+		y[1]=20;
+		y[2]=30;
+		y[3]=40;
+		y[4]=50;
+		y[5]=60;
+		y[6]=70;
+		
+		System.out.println(y[0]);
+	}
+}
