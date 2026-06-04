bf4c3bf7aa4fcdb1140b0f82ab27fefde01d2ee3
J.Thibakar <thiba2006077@gmail.com>
2026-06-04 11:55:45 +0530

    Create ArrayWhileLoopForward.java

---
+++ Task30_Accessing while loop using array/ArrayWhileLoopForward.java
@@ -0,0 +1,11 @@
+class ArrayWhileLoopForward{
+	public static void main(String args[]){
+		int[] x={10,20,30,40,50,60,70,80,90,100};
+		
+		int i = 0;
+		while(i < x.length){
+			System.out.println(x[i]);
+			i++;
+		}
+	}
+}
