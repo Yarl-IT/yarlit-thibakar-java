c1c7bf795d6dfac121d31586a1a055485172543e
J.Thibakar <thiba2006077@gmail.com>
2026-07-27 21:39:52 +0530

    Create StarPattern2.java

---
+++ Task55_ Inverted Right-Angled Star Triangle/StarPattern2.java
@@ -0,0 +1,13 @@
+public class StarPattern2 {
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        for (int i = x; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
