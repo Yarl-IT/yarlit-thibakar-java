5adf37af93a0de030c258e5fb932d7790b2386d5
J.Thibakar <thiba2006077@gmail.com>
2026-08-13 10:38:18 +0530

    Create StarPatteren5.java

---
+++ Task 58_Star Pyramid/StarPatteren5.java
@@ -0,0 +1,20 @@
+class StarPatteren5{
+    public static void main(String args[]){
+        int x = 5; 
+
+        for (int i = 1; i <= x;  i++){
+            
+          
+            for (int j = i; j <x; j++){
+                System.out.print("  "); 
+            }
+    
+            for (int k = 1; k <= (2*i-1); k++){
+                System.out.print("* ");
+            }
+            
+           
+            System.out.println();
+        }
+    }
+}
